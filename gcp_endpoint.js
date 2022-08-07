2c3e4ec8c3210624b7749348c1a9b6072202011d
br0steen <osteensco@gmail.com>
2022-08-06 21:08:17 -0400

    added webhook endpoint for gcp

---
+++ gcp_endpoint.js
@@ -0,0 +1,34 @@
+const functions = require('@google-cloud/functions-framework')
+
+// Register an HTTP function with the Functions Framework
+functions.http('myHttpFunction', (req, res) => {
+
+    // const topicNameOrId = 'YOUR_TOPIC_NAME_OR_ID'
+    const data = JSON.stringify(req)
+
+    // Imports the Google Cloud client library
+    const {PubSub} = require('@google-cloud/pubsub')
+
+    // Creates a client; cache this for further use
+    const pubSubClient = new PubSub()
+
+    async function publishMessage() {
+    // Publishes the message as a string
+        const dataBuffer = Buffer.from(data)
+
+        try {
+            const messageId = await pubSubClient
+            .topic(topicNameOrId)
+            .publishMessage({data: dataBuffer})
+            console.log(`Message ${messageId} published.`)
+        } catch (error) {
+            console.error(`Received error while publishing: ${error.message}`)
+            process.exitCode = 1
+        }
+    }
+
+    publishMessage()
+
+    // Send an HTTP response
+    res.send('OK')
+})
