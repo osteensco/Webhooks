2a475119e75fd3057582d998559aa763086fa310
br0steen <osteensco@gmail.com>
2023-06-26 14:55:51 -0500

    csrf fix update

--- site_traffic.js
+++ site_traffic.js
@@ -15,6 +15,20 @@ function setExpiration(type) {
 
 }
 
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) {
+        return parts.pop().split(';').shift();
+    }
+    return null;
+}
+
+
+
+
+
+
 class Cookie {
     constructor (type) {
         this.type = type
@@ -94,15 +108,12 @@ class Cookie {
 class Webhook {
     constructor (data, endpoint){
         this.payload = data
-        this.endpoint = this.build(endpoint)
-        this.send(data, this.endpoint)
+        this.build(endpoint)
+        this.send(data, "/webhooks/")
     }
 
     build (endpoint) {
-        let ep = document.getElementById(endpoint)
-        let url = ep.value
-        ep.remove()
-        return url
+        this.payload.webhook = endpoint
     }
 
     send (data, endpointURL) {
@@ -111,19 +122,29 @@ class Webhook {
   
         request.setRequestHeader('Content-type', 'application/json')
 
-        request.send(JSON.stringify(data))
+        const csrftoken = getCookie('csrftoken');
+
+        request.setRequestHeader('X-CSRFToken', csrftoken);
+    
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE) {
+                if (request.status === 200) {
+                    console.log('Data forwarded successfully');
+                } else {
+                    console.error('Error forwarding data:', request.status);
+                }
+            }
+        };
+    
+        request.send(JSON.stringify(data));
 
     }
-
-
-
 }
 
 
 
 
-
-///////////////scripts/////////////////
+///////////////script/////////////////
 
 let id = new Cookie('id')
 let session = new Cookie('sessionID')
