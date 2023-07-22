fee9a7a193011dc7698b3cb451bd67e5578ad04f
br0steen <osteensco@gmail.com>
2023-07-22 09:15:22 -0500

    pwr5teamsAPI

---
+++ college_football_teams/pwr5teamsAPI.py
@@ -0,0 +1,35 @@
+from google.cloud import bigquery
+import json
+import os
+
+
+
+
+
+def main(request):
+
+    client = bigquery.Client()
+
+    TABLE_PATH = os.environ.get('TABLE_PATH')
+
+    conference = request.args.get('conference', default=None, type=str).lower()
+
+    if conference == 'all':
+        query = f"""
+            SELECT * FROM {TABLE_PATH}
+        """
+    else:
+        query = f"""
+            SELECT *
+            FROM {TABLE_PATH}
+            WHERE LOWER(GameID) = @conference
+        """
+
+    query_job = client.query(query, conference=conference)
+    results = query_job.result()
+
+    data = [dict(row) for row in results]
+
+    return json.dumps(data)
+
+
